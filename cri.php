a37403910a5f7edfe772f79b523cf4759e8531ac
DebiYohana <163828029+DebiYohana@users.noreply.github.com>
2024-03-18 15:26:59 +0700

    Add files via upload

---
+++ cri.php
@@ -0,0 +1,11 @@
+<html>
+<head> <title> curiculum vitae debi yohana </title> </head>
+<BODY BGCOLOR=pink>
+<h1 align="center">CURICULUM VITAE</h1> <HR>
+<font size=4><DIV ALIGN="left">Nama saya Debi yohana. Saya lahir di kota Bandung, sebuah kota<br> 
+yang sejuk dan indah serta dikelilingi gunung-gunung.<br>
+bandung terkenal dengan kota kembang<br>
+Yang jelas, Bandung merupakan kota yang selalu memberikan kenangan yang tak<br> 
+akan terlupakan sepanjang masa.<br>
+</BODY> 
+</HTML>
